da74acf721ba2d6e7e8d5796733289bd4d125d8c
Fmanzi <fmanzi350@gmail.com>
2023-10-19 17:05:04 -0700

    push pall

--- monty_pall.c
+++ monty_pall.c
@@ -1,14 +1,18 @@
 #include "monty.h"
 
+/**
+ * pall - Prints all elements of the stack
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number (unused)
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    (void)line_number; /* Unused parameter */
-
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	(void)line_number;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
--- monty_push.c
+++ monty_push.c
@@ -1,29 +1,32 @@
 #include "monty.h"
 
+/**
+ * push - Pushes a new node onto the stack
+ * @stack: Double pointer to the top of the stack
+ * @value: Value to be stored in the new node
+ */
 void push(stack_t **stack, unsigned int value)
 {
-    stack_t *new_node;
+	stack_t *new_node;
 
-    if (!stack)
-    {
-        /* Handle error: no stack provided */
-        fprintf(stderr, "Error: no stack provided\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!stack)
+	{
+		fprintf(stderr, "Error: no stack provided\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        /* Handle error: malloc failed */
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
